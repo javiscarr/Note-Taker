150ae2c700722957ddd84e30f7fc020cc73c0bf0
Doonywoony <javismcarr@gmail.com>
2022-01-22 22:53:50 -0500

    hmtlRouters added

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+//dependencies
+
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public.index.html'));
+
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+module.exports = router;
