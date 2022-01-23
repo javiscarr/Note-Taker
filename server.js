4a272003825dd07e4b169f5398db5caf3187ed07
Doonywoony <javismcarr@gmail.com>
2022-01-22 19:39:23 -0500

    server.js completed

--- server.js
+++ server.js
@@ -6,3 +6,27 @@ const express = require('express');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+//express server is created
+
+const app = express();
+
+//setting PORT
+
+const PORT = process.env.PORT || 3001;
+
+//setting Express app up to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//Listen
+
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
+});
+
+
