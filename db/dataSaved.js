fc8a85b84c71b191f45dd96c5073e37d996b8039
Doonywoony <javismcarr@gmail.com>
2022-01-22 23:30:49 -0500

    delete functionality added

--- db/dataSaved.js
+++ db/dataSaved.js
@@ -43,4 +43,15 @@ class Save {
             .then(updatedNotes => this.write(updatedNotes))
             .then(() => newNote);
     }
+
+    //deletes a note
+
+    deleteNotes(id) {
+        return this.returnNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
+
+    }
 }
+
+module.exports = new Save();
