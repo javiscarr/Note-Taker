b7a111656b0c5f7fc851fb1538b89f1f8a3bffc9
Doonywoony <javismcarr@gmail.com>
2022-01-23 00:51:40 -0500

    note deletion, ability to clear and add new notes, show and hide save button, show titles list, and show and hide delete button features created

--- public/assets/index.js
+++ public/assets/index.js
@@ -83,5 +83,81 @@ const confirmNoteSave = () => {
     });
 };
 
+//note deletion
+
+const handleNoteDelete = (e) => {
+    //prevents the click listener from being called when the button inside is clicked
+    e.stopPropagation();
+
+    const note = e.target;
+    const notId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+
+    if (activeNote.id === noteId) {
+        activeNote = {};
+    }
+
+    deleteNote(noteId).then(() => {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
+};
+
+//Clears active note and gives the user the ability to add a new note
+const handleNewNoteView = (e)  => {
+    activeNote = {};
+    renderActiveNote();
+};
+
+const handleRenderSaveBtn = () => {
+    if (!noteTitle.value.trim() || !noteText.value.trim()) {
+        hide(savedNoteBtn);
+    } else {
+        show(savedNoteBtn);
+    }
+};
+
+//shows list note titles
+const renderNoteList = async (notes) => {
+    let jsonNotes = await notes.json();
+    if (window.location.pathname === '/notes') {
+        noteList.forEach((el) => (el.innerHTML = ''));
+    }
+
+    let noteListItems = [];
+
+    //returns HTML element with or without a button to delete
+
+    const createLi = (text, delBtn = true) => {
+        const liEl = document.createElement('li');
+        liEl.classList.add('list=group=item');
+
+        const spanEl = document.createElement('span');
+        spanEl.innerText = text;
+        spanEl.addEventListener('click', handleNewNoteView);
+
+        liEl.append(spanEl);
+
+        if (delBtn) {
+            const delBtnEl = document.createElement('i');
+            delBtnEl.classList.add (
+                'fas',
+                'fa-trash-alt',
+                'float-right',
+                'text-danger',
+                'delete-note'
+            );
+            delBtnEl.addEventListener('click', handleNewNoteDelete);
+
+            liEl.append(delBtnEl);
+        }
+
+        return liEl;
+
+    };
+}
+
+
+
+
 
 
