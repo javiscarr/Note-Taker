c8a70a61476d313d60fe2f2ba8a8a618a4ac864a
Doonywoony <javismcarr@gmail.com>
2022-01-22 19:09:32 -0500

    created apiRoutes.js and htmlRoutes.js files

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependency
+
+const express = require('express');
+
+//directs server to route files
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
